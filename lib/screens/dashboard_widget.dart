ee34c7543d457e8cf167740b2eea4202d5d8ea68
reticent-roklimber <dev.equinoct@gmail.com>
2024-04-06 12:36:44 +0530

    Minor changes to dashboard and floating camera widgets

---
+++ lib/screens/dashboard_widget.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+import 'package:maplibre_gl/maplibre_gl.dart';
--- lib/widgets/floating_camera_widget.dart
+++ lib/widgets/floating_camera_widget.dart
@@ -4,7 +4,7 @@ import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 class FloatingCameraWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SpeedDial(children: [
+    return SpeedDial(icon: Icons.camera, children: [
       SpeedDialChild(child: Icon(Icons.camera), label: 'Camera'),
       SpeedDialChild(child: Icon(Icons.browse_gallery), label: 'Gallery'),
     ]);

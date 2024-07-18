85226f49065c3d4d793fd59466dea6029857c84a
reticent-roklimber <dev.equinoct@gmail.com>
2024-07-18 16:40:51 +0530

    Adding components to build station preferences page

---
+++ lib/screens/station_prefs_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutterflow_ui/flutterflow_ui.dart';
+
+// import 'package:gather/models/StationPrefsModel';
+
+class StationPrefsPage extends StatefulWidget {
+  @override
+  State<StationPrefsPage> createState() => _StationPrefsPageState();
+}
+
+class _StationPrefsPageState extends State<StationPrefsPage> {
+  @override
+  void initState() {
+    super.initState();
+    // _model = createModel(context, () => StationPrefsModel());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+}

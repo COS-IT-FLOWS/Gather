e14315598377a482360bcf13620fca84261033fb
reticent-roklimber <dev.equinoct@gmail.com>
2026-01-21 15:45:41 +0530

    Misc changes + Increase build number from 2 to 3 + Add aider to .gitignore + Replace CardTheme with CardThemeData in app_theme.dart

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -45,7 +45,7 @@ class AppTheme {
           elevation: 2,
         ),
         // Card theme
-        cardTheme: CardTheme(
+        cardTheme: CardThemeData(
           elevation: 2,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(8),
